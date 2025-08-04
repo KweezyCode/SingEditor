aa08e275649cacae9d0a7417bdcb930bee056b76
KweezyCode <unknown2w+github@gmail.com>
2025-08-04 20:10:16 +0300

    Add routing and network configuration fields to ListenFields class

--- src/main/java/com/kweezy/singeditor/config/common/ListenFields.java
+++ src/main/java/com/kweezy/singeditor/config/common/ListenFields.java
@@ -8,7 +8,51 @@ import lombok.*;
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ListenFields {
-    private Integer mark;
     @JsonProperty("bind_interface")
-    private String  bindInterface;
+    private String bindInterface;
+
+    @JsonProperty("routing_mark")
+    private Integer routingMark;
+
+    @JsonProperty("reuse_addr")
+    private Boolean reuseAddr;
+
+    @JsonProperty("netns")
+    private String netns;
+
+    @JsonProperty("tcp_fast_open")
+    private Boolean tcpFastOpen;
+
+    @JsonProperty("tcp_multi_path")
+    private Boolean tcpMultiPath;
+
+    @JsonProperty("udp_fragment")
+    private Boolean udpFragment;
+
+    @JsonProperty("udp_timeout")
+    private String udpTimeout;
+
+    @JsonProperty("detour")
+    private String detour;
+
+    /** @deprecated inbound fields are deprecated and will be removed in sing-box 1.13.0 */
+    @Deprecated
+    @JsonProperty("sniff")
+    private Boolean sniff;
+
+    @Deprecated
+    @JsonProperty("sniff_override_destination")
+    private Boolean sniffOverrideDestination;
+
+    @Deprecated
+    @JsonProperty("sniff_timeout")
+    private String sniffTimeout;
+
+    @Deprecated
+    @JsonProperty("domain_strategy")
+    private String domainStrategy;
+
+    @Deprecated
+    @JsonProperty("udp_disable_domain_unmapping")
+    private Boolean udpDisableDomainUnmapping;
 }
